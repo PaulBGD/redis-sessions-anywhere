ef903ed97ea8278616fcfc7e20e031ef5063856c
Paul Sauve <paul@burngames.net>
2016-01-24 14:32:04 -0600

    Convert to es5 for testing

--- test/test.js
+++ test/test.js
@@ -2,115 +2,104 @@
 
 require('source-map-support').install();
 
-let assert = require('assert');
-let redis = require('redis');
-let RedisSessions = require('../index.js');
-let TokenGenerator = RedisSessions.TokenGenerator;
+var assert = require('assert');
+var redis = require('redis');
+var RedisSessions = require('../index.js');
+var TokenGenerator = RedisSessions.TokenGenerator;
 
-describe('redis-sessions-anywhere', () => {
-    let client = redis.createClient();
-    let token = Math.random().toString();
+describe('redis-sessions-anywhere', function () {
+    var client = redis.createClient();
+    var token = Math.random().toString();
 
-    describe('simple', () => {
+    describe('simple', function () {
         // todo split these up?
-        it('can run a simple set of tests', () => {
-            let sessions = new RedisSessions(client);
-            return sessions.set(token, {dummy: true})
-                .then((object) => {
-                    assert(object, 'An object is not returned');
-                    assert(object.data, 'The object does not contain the data');
-                    assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
-                    assert.equal(object.timeLeft, sessions.options.ttl, 'The time left is not equal to the ttl');
+        it('can run a simple set of tests', function () {
+            var sessions = new RedisSessions(client);
+            return sessions.set(token, { dummy: true }).then(function (object) {
+                assert(object, 'An object is not returned');
+                assert(object.data, 'The object does not contain the data');
+                assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
+                assert.equal(object.timeLeft, sessions.options.ttl, 'The time left is not equal to the ttl');
 
-                    return sessions.get(token);
-                })
-                .then((object) => {
-                    assert(object, 'An object is not returned');
-                    assert(object.data, 'The object does not contain the data');
-                    assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
-                    assert(object.timeLeft <= sessions.options.ttl, 'The time left is more than the ttl');
-                    assert(object.timeLeft > 0, 'The time left is less than 0');
+                return sessions.get(token);
+            }).then(function (object) {
+                assert(object, 'An object is not returned');
+                assert(object.data, 'The object does not contain the data');
+                assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
+                assert(object.timeLeft <= sessions.options.ttl, 'The time left is more than the ttl');
+                assert(object.timeLeft > 0, 'The time left is less than 0');
 
-                    return sessions.delete(token);
-                })
-                .then(() => {
-                    return sessions.get(token);
-                })
-                .then((object) => {
-                    assert(object === null, 'Returns null');
-                })
-                .catch(err => {
-                    throw err;
-                });
+                return sessions['delete'](token);
+            }).then(function () {
+                return sessions.get(token);
+            }).then(function (object) {
+                assert(object === null, 'Returns null');
+            })['catch'](function (err) {
+                throw err;
+            });
         });
 
         // todo do actual tests on locking
-        it('can run the same tests but with locking', () => {
-            let sessions = new RedisSessions(client, {
+        it('can run the same tests but with locking', function () {
+            var sessions = new RedisSessions(client, {
                 lock: true
             });
-            return sessions.set(token, {dummy: true})
-                .then((object) => {
-                    assert(object, 'An object is not returned');
-                    assert(object.data, 'The object does not contain the data');
-                    assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
-                    assert.equal(object.timeLeft, sessions.options.ttl, 'The time left is not equal to the ttl');
+            return sessions.set(token, { dummy: true }).then(function (object) {
+                assert(object, 'An object is not returned');
+                assert(object.data, 'The object does not contain the data');
+                assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
+                assert.equal(object.timeLeft, sessions.options.ttl, 'The time left is not equal to the ttl');
 
-                    return sessions.get(token);
-                })
-                .then((object) => {
-                    assert(object, 'An object is not returned');
-                    assert(object.data, 'The object does not contain the data');
-                    assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
-                    assert(object.timeLeft <= sessions.options.ttl, 'The time left is more than the ttl');
-                    assert(object.timeLeft > 0, 'The time left is less than 0');
+                return sessions.get(token);
+            }).then(function (object) {
+                assert(object, 'An object is not returned');
+                assert(object.data, 'The object does not contain the data');
+                assert(object.data.dummy, 'The dummy value is not set ' + JSON.stringify(object));
+                assert(object.timeLeft <= sessions.options.ttl, 'The time left is more than the ttl');
+                assert(object.timeLeft > 0, 'The time left is less than 0');
 
-                    return sessions.delete(token);
-                })
-                .then(() => {
-                    return sessions.get(token);
-                })
-                .then((object) => {
-                    assert(object === null, 'Returns null');
-                })
-                .catch(err => {
-                    throw err;
-                });
+                return sessions['delete'](token);
+            }).then(function () {
+                return sessions.get(token);
+            }).then(function (object) {
+                assert(object === null, 'Returns null');
+            })['catch'](function (err) {
+                throw err;
+            });
         });
     });
 
-    describe('token generation', () => {
-        it('can generate a token then check it', () => {
-            let sessions = new RedisSessions(client);
-            let generator = new TokenGenerator(sessions, {
+    describe('token generation', function () {
+        it('can generate a token then check it', function () {
+            var sessions = new RedisSessions(client);
+            var generator = new TokenGenerator(sessions, {
                 key: 'adwadwadawdawdawdawadwadaaaaaaaaaaaaaaaaaaaaaaaaaaawdwadwwaddawdadw',
                 checkForCollission: false // we're not testing this right now
             });
-            return generator.generateKey().then(key => {
+            return generator.generateKey().then(function (key) {
                 assert.notEqual(null, key, 'key is not returned');
                 assert(key.token.length === 32, 'length of token is not 32');
-                let parsed = generator.parseClientToken(key.clientToken);
+                var parsed = generator.parseClientToken(key.clientToken);
                 assert(parsed.token === key.token, 'key is not the same');
                 assert(Date.now() - parsed.expiresAt <= sessions.options.ttl, 'the time left is more than the ttl');
                 assert(generator.isValid(key.clientToken), 'the token is not valid');
             });
         });
 
-        it('can check for collission', () => {
-            let sessions = new RedisSessions(client);
-            let generator = new TokenGenerator(sessions, {
+        it('can check for collission', function () {
+            var sessions = new RedisSessions(client);
+            var generator = new TokenGenerator(sessions, {
                 key: 'adwadwadawdawdawdawadwadaaaaaaaaaaaaaaaaaaaaaaaaaaawdwadwwaddawdadw'
             });
             return generator.generateKey();
         });
     });
 
-    afterEach((done) => {
+    afterEach(function (done) {
         client.del(['resa:' + token, 'resa:' + token + ':lock'], done);
     });
 
-    after(() => {
+    after(function () {
         client.quit();
     });
-
 });
