130d662e6af4bde5340a9fb4803d1487d3f275f9
Paul Sauve <paul@burngames.net>
2016-01-24 19:10:43 -0600

    Remove typings

--- typings/tsd.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="bluebird/bluebird.d.ts" />
-/// <reference path="debug/debug.d.ts" />
-/// <reference path="express/express.d.ts" />
-/// <reference path="mime/mime.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="redis/redis.d.ts" />
-/// <reference path="serve-static/serve-static.d.ts" />
